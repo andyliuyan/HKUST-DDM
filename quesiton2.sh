80476452dda39229c570087049368192a790f11e
andyliuyan <61860151+andyliuyan@users.noreply.github.com>
2020-10-19 23:30:49 +0800

    Update quesiton2.sh

--- quesiton2.sh
+++ quesiton2.sh
@@ -2,9 +2,5 @@
 for a in {1..100}
 do
 	mkdir DDM$a
-	cd DDM$a
-	touch time_till_now.txt
-	echo "nanoseconds since 1970-01-01 00:00:00 UTC:">time_till_now.txt
-	echo "<`date +%s`>">>time_till_now.txt
-	cd ../
+	echo "nanoseconds since 1970-01-01 00:00:00 UTC:\n<"`date +%s%N`">" > "DDM$a/time_till_now.txt"
 done
